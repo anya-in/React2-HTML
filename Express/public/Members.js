a48e664b2981e5fc87f76af620316b2d6fe38059
Lavanya Lahari <lavanyalaharikalla@gmail.com>
2022-07-16 21:30:36 +0530

    init2

---
+++ Express/public/Members.js
@@ -0,0 +1,23 @@
+
+const members = [
+    {
+        id: 1,
+        name: 'Tara Green',
+        email: 'taragreen@xyz',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Sara Green',
+        email: 'saragreen@xyz',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'Anya Inara',
+        email: 'anya@xyz',
+        status: 'active'
+    }
+];
+
+module.exports = members;
