26518a4981cd337655cc2a12ebdaf9228a41780a
Lavanya Lahari <lavanyalaharikalla@gmail.com>
2022-07-21 12:29:13 +0530

    init

--- NodeJS&Express/app.js
+++ NodeJS&Express/app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { body, validationResult } = require('express-validator');
+//const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 mongoose.connect('mongodb://localhost/nodekb');
 let db = mongoose.connection;
@@ -17,6 +21,8 @@ db.on('error', function (err) {
 
 //Init App
 const app = express();
+var router = express.Router();
+
 
 //Bring in Models
 let Article = require('./models/article');
@@ -35,6 +41,48 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Express session Middleware
+app.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true
+}));
+
+//Express Messages Middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+    res.locals.messages = require('express-messages')(req, res);
+    next();
+});
+
+//Express Validator Middleware
+app.use(express.json());
+app.post('/user', (req, res) => {
+    User.create({
+        username: req.body.username,
+        password: req.body.password,
+    }).then(user => res.json(user));
+});
+
+app.post(
+    '/user',
+    // username must be an email
+    body('username').isEmail(),
+    // password must be at least 5 chars long
+    body('password').isLength({ min: 5 }),
+    (req, res) => {
+        // Finds the validation errors in this request and wraps them in an object with handy functions
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        User.create({
+            username: req.body.username,
+            password: req.body.password,
+        }).then(user => res.json(user));
+    },
+);
 //Home Route
 app.get('/', (req, res) => {
     Article.find({}, (err, articles) => {
@@ -49,26 +97,6 @@ app.get('/', (req, res) => {
         }
 
     });
-    //let articles = [
-    //         {
-    //             id: 1,
-    //             title: 'Article One',
-    //             author: 'Tara Green',
-    //             body: 'This is article One'
-    //         },
-    //         {
-    //             id: 2,
-    //             title: 'Article Two',
-    //             author: 'Tara Green',
-    //             body: 'This is article Two'
-    //         },
-    //         {
-    //             id: 3,
-    //             title: 'Article Three',
-    //             author: 'Tara Green',
-    //             body: 'This is article three'
-    //         }
-    //     ];
 
 })
 
@@ -91,17 +119,31 @@ app.get('/articles/add', (req, res) => {
 
 //Add Submit POST Route
 app.post('/articles/add', (req, res) => {
+    req.checkBody('title', 'Title is required').notEmpty;
+    req.checkBody('author', 'Author is required').notEmpty;
+    req.checkBody('body', 'Body is required').notEmpty;
+    //Get Errors
+    let errors = req.validationErrors();
+    if (errors) {
+        res.render('add_article', {
+            title: 'Add article',
+            errors: errors
+        });
+    } else {
+
+    }
     //submitting to db
     let article = new Article();
     article.title = req.body.title;
     article.author = req.body.author;
     article.body = req.body.body;
-    article.save(err => {
+    article.save(function (err) {
         if (err) {
             console.log(err);
             return;
 
         } else {
+            req.flash('success', 'Article Added');
             res.redirect('/');
         }
     });
@@ -132,6 +174,7 @@ app.post('/articles/edit/:id', (req, res) => {
             return;
 
         } else {
+            req.flash('success', 'Article Updated');
             res.redirect('/');
         }
     });
