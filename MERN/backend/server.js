05986533f5d7e054fef6de6d0964d958ea205aea
Lavanya Lahari <lavanyalaharikalla@gmail.com>
2022-08-13 18:50:27 +0530

    init

---
+++ MERN/backend/server.js
@@ -0,0 +1,11 @@
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const bodyParser = require('body-parser');
+
+// const app = express();
+
+
+// //Bodyparser Middleware
+// app.use(bodyParser.json());
+
+console.log('Hello World');
