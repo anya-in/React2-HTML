dbf79d4402706e9d2e52a1d3a1ba71f6918d2351
Lavanya Lahari <lavanyalaharikalla@gmail.com>
2022-08-17 14:52:04 +0530

    goals controller and routes setup

--- MERN/backend/server.js
+++ MERN/backend/server.js
@@ -1,11 +1,16 @@
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const bodyParser = require('body-parser');
+const express = require('express');
+const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const port = process.env.PORT || 5000
 
-// const app = express();
+const app = express();
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
-// //Bodyparser Middleware
-// app.use(bodyParser.json());
+app.use('/api/goals', require('./routes/goalRoutes'))
 
-console.log('Hello World');
+app.use(errorHandler)
+
+//Bodyparser Middleware
+app.listen(port, () => console.log(`Server started on port ${port}`));
