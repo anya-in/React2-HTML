ff754bb562402d8a1aa623a16e186d575d074997
Lavanya Lahari <lavanyalaharikalla@gmail.com>
2022-08-18 19:17:25 +0530

    JWT Authentication

--- MERN/backend/controllers/goalcontroller.js
+++ MERN/backend/controllers/goalcontroller.js
@@ -1,12 +1,14 @@
 const asyncHandler = require('express-async-handler')
 
 const Goal = require('../models/goalModel')
+const Users = require('../models/userModel')
+
 
 //@desc Get Goals
 //@route Get /api/goals
 // @access Private
 const getGoals = asyncHandler(async (req, res) => {
-    const goals = await Goal.find()
+    const goals = await Goal.find({ user: req.user.id })
     res.status(200).json(goals)
 
 })
@@ -21,7 +23,8 @@ const setGoals = asyncHandler(async (req, res) => {
 
     };
     const goal = await Goal.create({
-        text: req.body.text
+        text: req.body.text,
+        user: req.user.id
     })
     res.status(200).json(goal)
 
@@ -39,6 +42,19 @@ const updateGoals = asyncHandler(async (req, res) => {
         throw new Error('Goal not found')
     }
 
+    const user = await Users.findById(req.user.id)
+    //check for user
+    if (!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+    //Make sure logged in user matches goal user
+    if (goal.user.toString() !== user.id) {
+        res.status(401)
+        throw new Error('User not authorised')
+    }
+
+
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
         new: true,
     })
@@ -54,6 +70,17 @@ const deleteGoals = asyncHandler(async (req, res) => {
     if (!goal) {
         res.status(400)
         throw new Error('Goal not found')
+    }
+    const user = await Users.findById(req.user.id)
+    //check for user
+    if (!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+    //Make sure logged in user matches goal user
+    if (goal.user.toString() !== user.id) {
+        res.status(401)
+        throw new Error('User not authorised')
     }
 
     await goal.remove()
